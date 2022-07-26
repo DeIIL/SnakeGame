ec7c4693976827ab80373713bc1ca58933fe96d5
Gustavo Del Castillo <gudellcastillo@gmail.com>
2022-07-26 17:51:13 -0300

    including project

---
+++ src/SnakeGame.java
@@ -0,0 +1,13 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		/*Shortcut para -> GameFrame frame = new GameFrame() <- como nesse projeto nao vai ter 
+		outro uso pra essa instancia(por exemplo usar a instacia em outra classe), 
+		nao é necessario por exemplo nomear a instancia
+		*/
+		new GameFrame();
+
+	}
+
+}
